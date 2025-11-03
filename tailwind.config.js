44af73feb25a4a8b4764c98a9622a965e5fdcf05
Amadeo Bon <amadeosph@outlook.com>
2025-11-03 16:39:46 -0300

    Refactor PostCSS and Tailwind CSS configuration for clarity and consistency

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,10 @@
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html","./**/*.{ts,tsx,js,jsx}"],
+  content: [
+    "./index.html",
+    "./**/*.{ts,tsx,js,jsx}"
+  ],
   theme: { extend: {} },
   plugins: [],
 }
+
